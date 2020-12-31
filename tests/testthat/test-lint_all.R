826a93bad4dce3f130016cb18f98ff10c063491b
John Benninghoff <jbenninghoff@mac.com>
2020-12-31 11:23:59 -0600

    Add note on potentially unsafe tests

--- tests/testthat/test-lint_all.R
+++ tests/testthat/test-lint_all.R
@@ -1,3 +1,5 @@
+# these tests currently run against the files in tests/testthat : this is potentially dangerous
+# and should be changed
 test_that("returns the correct type", {
   expect_equal(typeof(lint_all()), "list")
 })
