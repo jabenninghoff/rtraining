d1be5bf7545ed3b345b2d88e5e7334423b7cba55
John Benninghoff <jbenninghoff@mac.com>
2020-12-31 11:32:12 -0600

    Add reference to comment on tests

--- tests/testthat/test-lint_all.R
+++ tests/testthat/test-lint_all.R
@@ -1,5 +1,5 @@
 # these tests currently run against the files in tests/testthat : this is potentially dangerous
-# and should be changed
+# and should be changed - see https://r-pkgs.org/tests.html
 test_that("returns the correct type", {
   expect_equal(typeof(lint_all()), "list")
 })
