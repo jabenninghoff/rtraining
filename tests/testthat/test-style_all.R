f41bd535e36f40d78b9663e4e240f9d458be046d
John Benninghoff <jbenninghoff@mac.com>
2020-12-31 11:23:59 -0600

    Add note on potentially unsafe tests

--- tests/testthat/test-style_all.R
+++ tests/testthat/test-style_all.R
@@ -1,3 +1,5 @@
+# these tests currently run against the files in tests/testthat : this is potentially dangerous
+# and should be changed
 test_that("returns a tibble", {
   expect_equal(tibble::is_tibble(style_all()), TRUE)
 })
