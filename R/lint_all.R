4cddecd8a27719c773120cccfa8394d979227587
John Benninghoff <jbenninghoff@mac.com>
2020-12-31 11:05:06 -0600

    Add packrat to lint_all exclusions, run styler

--- R/lint_all.R
+++ R/lint_all.R
@@ -1,7 +1,8 @@
 #' Lint all files
 #'
 #' Lint all files in a project. Implemented as a wrapper for lintr::lint_dir() that excludes .git,
-#' .github, and renv by default.
+#' .github, packrat, and renv by default.
+#'
 #' @param path the path to the base directory, by default,
 #' it will be searched in the parent directories of the current directory.
 #' @param exclusions exclusions for \code{\link[lintr]{exclude}}, relative to the
@@ -9,15 +10,15 @@
 #' @return A list of lint objects.
 #' @examples
 #' \dontrun{
-#'   lint_all()
-#'   lint_all("notebooks")
-#'   lint_all(
-#'     path = "./inst",
-#'     exclusions = list("inst/example/bad.R")
-#'   )
+#' lint_all()
+#' lint_all("notebooks")
+#' lint_all(
+#'   path = "./inst",
+#'   exclusions = list("inst/example/bad.R")
+#' )
 #' }
 #' @import lintr
 #' @export
-lint_all <- function(path = ".", exclusions = list(".git", ".github", "renv")) {
+lint_all <- function(path = ".", exclusions = list(".git", ".github", "packrat", "renv")) {
   lintr::lint_dir(path = path, exclusions = exclusions)
 }
