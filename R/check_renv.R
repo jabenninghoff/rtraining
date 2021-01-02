04d8e82945311c9f595a56a1e1749306c6167b5f
John Benninghoff <jbenninghoff@mac.com>
2021-01-01 18:29:06 -0600

    Implement check_renv()

---
+++ R/check_renv.R
@@ -0,0 +1,25 @@
+#' Check renv
+#'
+#' Checks \code{renv} \code{status()}, \code{clean()}, and optionally \code{update()}
+#'
+#' @param update run \code{renv::update} (ON by default)
+#'
+#' @import renv
+#' @export check_renv
+#' @examples
+#' \dontrun{
+#' check_renv()
+#' check_renv(update = FALSE)
+#' }
+check_renv <- function(update = TRUE) {
+  writeLines("renv::status()")
+  renv::status()
+
+  writeLines("\nrenv::clean()")
+  renv::clean()
+
+  if (update) {
+    writeLines("\nrenv::update()")
+    renv::update()
+  }
+}
